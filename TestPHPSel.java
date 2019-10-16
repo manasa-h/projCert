bda61b9b35a54125cbfc5f88cf8686ef7e2921a0
root <root@ip-172-31-90-128.ec2.internal>
2019-10-16 16:03:35 +0000

    adding selenium test class and java file

---
+++ TestPHPSel.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+ 
+public class TestPHPSel {
+ 
+        public static void main(String[] args) throws IOException, InterruptedException {
+                System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--no-sandbox");
+ 
+                WebDriver driver = new ChromeDriver(chromeOptions);
+ 
+                driver.get("http://34.238.255.214:32768/");
+ 
+                Thread.sleep(1000);
+
+		driver.findElement(By.linkText("About Us")).click();
+ 
+                if (driver.getPageSource().contains("Lorem Ipsum Dipsum")) {
+                        System.out.println("Pass");
+                } else {
+                        System.out.println("Fail");
+                }
+                driver.quit();
+        }
+}
+
